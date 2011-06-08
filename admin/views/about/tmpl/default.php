784589f0afb02093edfe85cc90a50655a7c77f94
Yaroslav Vorozhko <yaroslav@ivinco.com>
2011-06-08 14:14:27 +0200

    added about page

--- admin/views/about/tmpl/default.php
+++ admin/views/about/tmpl/default.php
@@ -4,3 +4,27 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 JToolBarHelper::title(JText::_('SphinxSearch About'), 'config.png');
 
 ?>
+
+Joomla Sphinx search component enables the Sphinx-powered search with high-performance and more relevant search results.
+
+<h4>Features:</h4>
+<ul>
+	<li>Google-like search syntax;</li>
+	<li>High searching performance;</li>
+	<li>Improved search result relevance;</li>
+	<li>Supports friendly URLs SEO settings;</li>
+	<li>Supports morphological word forms dictionaries;</li>
+	<li>Supports stemming (English, Russian and Czech are built-in);</li>
+	<li>Supports stopwords;</li>
+	<li>Supports both single-byte encodings and UTF-8;</li>
+	<li>And many other <a href="http://sphinxsearch.com/docs/current.html">Sphinx features</a></li>
+</ul>
+
+<h4>Requirements:</h4>
+<ul>
+	<li>Sphinx Search 0.9.9 or higher</li>
+	<li>Joomla 1.5</li>
+</ul>
+
+<h4>Support</h4>
+This component is developed by <a href="http://ivinco.com">Ivinco</a>. If you need commercial support, or if you’d like Joomla Sphinx Search component customized for your needs, <a href="http://ivinco.com/contact-us">we can help</a>.
